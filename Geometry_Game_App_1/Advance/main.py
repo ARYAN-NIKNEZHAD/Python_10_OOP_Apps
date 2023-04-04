0d60a52a8aef8bad72a1636d1caffdda5d9fff10
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-04 10:37:33 +0430

    Add-Adv_G1

---
+++ Geometry_Game_App_1/Advance/main.py
@@ -0,0 +1,17 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+my_turtle.penup()
+my_turtle.goto(50, 70)
+
+my_turtle.pendown()
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(200)
+my_turtle.left(90)
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(200)
+
+turtle.done()
+
