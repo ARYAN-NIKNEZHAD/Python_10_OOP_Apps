5d09214b95aac17b4d42daa96fa24f53824c4612
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-04 10:58:45 +0430

    Add-Geometry_game_1

---
+++ Geometry_Game_App_1/Advance/geometry_game_v1.py
@@ -0,0 +1,76 @@
+from random import randint
+import turtle
+
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.point1.x < self.x < rectangle.point2.x \
+                and rectangle.point1.y < self.y < rectangle.point2.y:
+            return True
+        else:
+            return False
+
+
+class Rectangle:
+
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def area(self):
+        return (self.point2.x - self.point1.x) * \
+               (self.point2.y - self.point1.y)
+
+
+class GuiRectangle(Rectangle):
+
+    def draw(self, canvas):
+        canvas.penup()
+        canvas.goto(50, 70)
+
+        canvas.pendown()
+        canvas.forward(100)
+        canvas.left(90)
+        canvas.forward(200)
+        canvas.left(90)
+        canvas.forward(100)
+        canvas.left(90)
+        canvas.forward(200)
+
+        turtle.done()
+
+
+gui_rectangle = GuiRectangle(Point(randint(0, 400), randint(0, 400)),
+                             Point(randint(10, 400), randint(10, 400)))
+
+my_turtle = turtle.Turtle()
+
+gui_rectangle.draw(canvas=my_turtle)
+print(gui_rectangle.area())
+
+# # Create rectangle object
+# rectangle = Rectangle(Point(randint(0, 400), randint(0, 400)),
+#                       Point(randint(10, 400), randint(10, 400)))
+#
+#
+# # Print rectangle coordinates
+# print("Rectangle Coordinates: ",
+#       rectangle.point1.x, ",",
+#       rectangle.point1.y, "and",
+#       rectangle.point2.x, ",",
+#       rectangle.point2.y
+#       )
+#
+#
+# # Get point and area from user
+# user_point = Point(float(input("Guess x: ")), float(input("Guess y: ")))
+# user_area = float(input("Guess rectangle area: "))
+#
+# # Print out the game result
+# print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
+# print("Your area was off by: ", rectangle.area() - user_area)
