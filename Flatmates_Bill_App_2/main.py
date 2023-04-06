4f8340bd0c4b3c839f645177a9b2e3d54f1863d2
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-06 10:55:18 +0430

    Add_changes_PdfReport

--- Flatmates_Bill_App_2/main.py
+++ Flatmates_Bill_App_2/main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+
+
 class Bill:
     """
     Object that contains data about a bill, such as
@@ -36,7 +39,22 @@ class PdfReport:
         self.file_name = file_name
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        pdf = FPDF(orientation="P", unit="pt", format="A4")
+        pdf.add_page()
+
+        # Add Title
+        pdf.set_font(family="Times", size=24, style="B")
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+
+        # Insert Period label and value
+        pdf.cell(w=100, h=40, txt="Period", border=1)
+        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+
+        # Insert name and due amount of the first flatmate
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
+        pdf.cell(w=150, h=40, txt=str(flatmate1.pays(bill, flatmate2)), border=1, ln=1)
+
+        pdf.output(self.file_name)
 
 
 the_bill = Bill(amount=120, period="March 2021")
@@ -45,3 +63,6 @@ marry = FlatMate(name="Marry", days_in_house=25)
 
 print(f"John pays: {john.pays(bill=the_bill, flatmate2=marry)}")
 print(f"Marry pays: {marry.pays(bill=the_bill, flatmate2=john)}")
+
+pdf_report = PdfReport("Report1.pdf")
+pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
