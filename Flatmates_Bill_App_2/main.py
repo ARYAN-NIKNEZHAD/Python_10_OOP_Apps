5901a0125b5d9683c3555dd78737c9f80e39c325
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-08 08:25:13 +0430

    Add_new_file

--- Flatmates_Bill_App_2/main.py
+++ Flatmates_Bill_App_2/main.py
@@ -1,32 +1,7 @@
 from fpdf import FPDF
 import webbrowser
 
-
-class Bill:
-    """
-    Object that contains data about a bill, such as
-    total amount and period of the bill
-    """
-
-    def __init__(self, amount, period):
-        self.amount = amount
-        self.period = period
-
-
-class FlatMate:
-    """
-    Creates a flatmate person who lives in the flat
-    and pays a share of the bill.
-    """
-
-    def __init__(self, name, days_in_house):
-        self.name = name
-        self.days_in_house = days_in_house
-
-    def pays(self, bill, flatmate2):
-        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
-        to_pay = bill.amount * weight
-        return to_pay
+from Flatmates_Bill_App_2.flat import Bill, FlatMate
 
 
 class PdfReport:
