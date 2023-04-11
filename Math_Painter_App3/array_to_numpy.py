1e7a913bac36937444b90e67ea59bbbf4aaf65b9
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-11 09:48:26 +0430

    Add-changes

--- Math_Painter_App3/array_to_numpy.py
+++ Math_Painter_App3/array_to_numpy.py
@@ -7,6 +7,10 @@ data = np.zeros((5, 4, 3), dtype=np.uint8)
 data[:] = [255, 255, 0]
 print(data)
 
+data[0:3, 0:2] = [255, 200, 233]
+data[3:4, 1:4] = [45, 3, 233]
+
+
 img = Image.fromarray(data, "RGB")
 img.save("canvas.png")
 
