3445020f3875912742133ed5b5d8bda3671685c7
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-20 10:39:28 +0430

    Add-methods

--- Math_Painter_App3/main.py
+++ Math_Painter_App3/main.py
@@ -1,34 +1,56 @@
-class Canvas:
+from PIL import Image
+import numpy as np
 
-    def __init__(self, a, b, color):
-        self.a = a
-        self.b = b
+
+class Canvas:
+    """Object where all shapes are going to be drawn"""
+
+    def __init__(self, height, width, color):
+        self.height = height
+        self.width = width
         self.color = color
 
-    def make(self):
-        pass
+        self.data = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        # Change [0, 0, 0] with user given values for color
+        self.data[:] = self.color
+
+    def make(self, image_path):
+        """Converts the current array into an image file"""
+        img = Image.fromarray(self.data, "RGB")
+        img.save(image_path)
 
 
 class Rectangle:
 
-    def __init__(self, x, y, a, b, color):
+    def __init__(self, x, y, height, width, color):
+        """A rectangle shape that can be drawn on a Canvas object"""
         self.x = x
         self.y = y
-        self.a = a
-        self.b = b
+        self.height = height
+        self.width = width
         self.color = color
 
     def draw(self, canvas):
-        pass
+        """Draws itself into the canvas"""
+        canvas.data[self.x:self.x + self.height, self.y:self.y + self.width] = self.color
 
 
 class Square:
 
-    def __init__(self, x, y, a, color):
+    def __init__(self, x, y, side, color):
         self.color = color
         self.x = x
         self.y = y
-        self.a = a
+        self.side = side
 
     def draw(self, canvas):
-        pass
+        """Draws itself into the canvas"""
+        canvas.data[self.x:self.x + self.side, self.y:self.y + self.side] = self.color
+
+
+canvas = Canvas(height=20, width=30, color=(255, 255, 255))
+rec1 = Rectangle(x=1, y=6, height=7, width=10, color=(100, 200, 125))
+rec1.draw(canvas)
+sq1 = Square(x=1, y=3, side=3, color=(0, 100, 222))
+sq1.draw(canvas)
+canvas.make("canvas.png")
